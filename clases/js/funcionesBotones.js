325470b4aa8e859891080d2f8b4a326897e31482
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-02-09 19:33:11 -0500

    actualize las clases, falta finaliozar funciones botones

---
+++ clases/js/funcionesBotones.js
@@ -0,0 +1,11 @@
+function mostrarInformacionDelCurso(){
+  document.getElementById("texto").innerHTML="vote por petro y no necesito tu aprobacion";
+}
+
+function mostrarCompetencias(){
+  document.getElementById("texto").innerHTML="vote por petro y no necesito tu aprobacion121";
+}
+
+function mostrarObjetivosGenerales(){
+  document.getElementById("texto").innerHTML="vote por petro";
+}
