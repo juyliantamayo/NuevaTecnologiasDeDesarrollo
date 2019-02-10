e7f2644d1893ecee7b8afc165a9d436d1323f2fa
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-02-09 20:37:07 -0500

    actualización funciones botones

--- clases/js/funcionesBotones.js
+++ clases/js/funcionesBotones.js
@@ -1,11 +1,30 @@
+var clic=1,click=1,cli=1;
 function mostrarInformacionDelCurso(){
-  document.getElementById("texto").innerHTML="vote por petro y no necesito tu aprobacion";
+  if(clic==1){
+   document.getElementById('infoCurso').style.display = 'block';
+   clic = clic + 1;
+   } else{
+    document.getElementById('infoCurso').style.display = 'none';
+    clic = 1;
+   }
 }
 
 function mostrarCompetencias(){
-  document.getElementById("texto").innerHTML="vote por petro y no necesito tu aprobacion121";
+  if(click==1){
+   document.getElementById('competencias').style.display = 'block';
+   click = click + 1;
+   } else{
+    document.getElementById('competencias').style.display = 'none';
+    click = 1;
+   }
 }
 
 function mostrarObjetivosGenerales(){
-  document.getElementById("texto").innerHTML="vote por petro";
+  if(cli==1){
+   document.getElementById('objetivoGeneral').style.display = 'block';
+   cli = cli + 1;
+   } else{
+    document.getElementById('objetivoGeneral').style.display = 'none';
+    cli = 1;
+   }
 }
